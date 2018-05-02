1752c2eae62e1cd554b9f8312d39666692cb6aef
AlexPan <ppgod@live.cn>
2018-05-02 17:41:22 +0800

    refactor blockchain and SPV service SDK interface

--- sdk/spvservice.go
+++ sdk/spvservice.go
@@ -1,16 +1,17 @@
 package sdk
 
 import (
-	"github.com/elastos/Elastos.ELA.SPV/db"
+	"github.com/elastos/Elastos.ELA.SPV/core"
+	"github.com/elastos/Elastos.ELA.SPV/store"
 
-	"github.com/elastos/Elastos.ELA/bloom"
-	"github.com/elastos/Elastos.ELA.Utility/p2p"
+	ela "github.com/elastos/Elastos.ELA/core"
+	"github.com/elastos/Elastos.ELA.Utility/common"
 )
 
 /*
 SPV service is a high level implementation with all SPV logic implemented.
 SPV service is extend from SPV client and implement Blockchain and block synchronize on it.
-With SPV service, you just need to implement your own DataStore and GetBloomFilter() method, and let other stuff go.
+With SPV service, you just need to implement your own HeaderStore and SPVHandler, and let other stuff go.
 */
 type SPVService interface {
 	// Start SPV service
@@ -19,15 +20,28 @@ type SPVService interface {
 	// Stop SPV service
 	Stop()
 
-	// Get the Blockchain instance.
-	// Blockchain will handle block and transaction commits,
-	// verify and store the block and transactions.
-	// If you want to add extra logic when new block or transaction comes,
-	// use Blockchain.AddStateListener() to register chain state callbacks
-	Blockchain() *Blockchain
+	// ReloadFilters is a trigger to make SPV service refresh the current
+	// transaction filer(in our implementation the bloom filter) in SPV service.
+	// This will call onto the GetAddresses() and GetOutpoints() method in SPVHandler.
+	ReloadFilter()
 
-	// Broadcast a message to the peer to peer network.
-	BroadCastMessage(message p2p.Message)
+	// SendTransaction broadcast a transaction message to the peer to peer network.
+	SendTransaction(ela.Transaction)
+}
+
+type SPVHandler interface {
+	// SPV handler is extend from blockchain state listener
+	core.StateListener
+
+	// GetAddresses returns all addresses stored in your data store.
+	GetAddresses() []*common.Uint168
+
+	// GetOutpoint returns all UTXO(Unspent Transaction Output)s stored in your data store
+	// Outpoint is the reference that the account balance came from,
+	// it includes previous transaction ID and output index.
+	// For balance synchronize, implement GetAddresses() is enough, in that case,
+	// only transactions packed in blocks can be received, others will not.
+	GetOutpoints() []*ela.OutPoint
 }
 
 /*
@@ -36,6 +50,6 @@ there are two implementations you need to do, DataStore and GetBloomFilter() met
 DataStore is an interface including all methods you need to implement placed in db/datastore.go.
 Also an sample APP spvwallet is contain in this project placed in spvwallet folder.
 */
-func GetSPVService(client SPVClient, database db.DataStore, getBloomFilter func() *bloom.Filter) (SPVService, error) {
-	return NewSPVServiceImpl(client, database, getBloomFilter)
+func GetSPVService(client SPVClient, headerStore store.HeaderStore, handler SPVHandler) (SPVService, error) {
+	return NewSPVServiceImpl(client, headerStore, handler)
 }
